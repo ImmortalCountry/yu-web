f89b5334da1a05d49822807da062f032b9b7f799
sundy8336 <sundy8336@fxiaoke.com>
2020-03-06 10:30:10 +0800

    init

--- src/components/Article/ArticleDetail.vue
+++ src/components/Article/ArticleDetail.vue
@@ -1,69 +1,61 @@
 <template>
-  <el-container class="home">
+  <el-container class="div-body">
     <!--    头-->
     <el-header>
       <Header></Header>
     </el-header>
-
-    <el-main class="main">
-      <div class="main-div">
-        <el-row :gutter="20">
-          <el-col :span="6">
-            <div class="header">
+    <div class="main">
+      <el-main class="main-box">
+        <el-row :gutter="20" style="height: 100%">
+          <el-col :span="5" style="height: 100%">
+            <div style="background-color: aqua" class="main-left"><span>左侧</span></div>
+          </el-col>
+          <el-col :span="12" style="height: 100%">
+            <div class="main-center">
               <el-row :gutter="20">
-                <div class="user-header">
-                  <el-col :span="2">
-                    <div style="height: 60px; width: 70px; margin-left: 20px; margin-top: 20px">
-                      <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-                                 style="height: 100%;width: 100%; cursor: pointer"></el-avatar>
-                    </div>
-                  </el-col>
-                </div>
-                <div class="user-header" style="height: 60px; width: 70px;">
-                  <el-col :span="2">
-                    <el-row>
-                      <el-col>
-                        <div style="width: 300px; margin-top: 25px">
-                          {{articleInfo.create_time}}
-                        </div>
-                      </el-col>
-                    </el-row>
-                    <el-row>
-                      <el-col>
-                        <div style="width: 300px; margin-top: 10px">{{authorInfo.name}}xiaoming</div>
-                      </el-col>
-                    </el-row>
-                  </el-col>
-                </div>
-                <div>
-                  <el-col :span="2" push="10" style="margin-top: 25px">
-                    <el-button type="success" size="medium">{{buttonInfo}}已关注</el-button>
-                  </el-col>
-                </div>
+                <el-col :span="3" style="margin-left: 10px;">
+                  <div class="center-header">
+                    <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                               style="height: 100%;width: 100%; cursor: pointer"></el-avatar>
+                  </div>
+                </el-col>
+                <el-col :span="15">
+                  <el-row :gutter="20" style="margin-top: 23px"><span>{{authorInfo.name}}小明</span></el-row>
+                  <el-row :gutter="20" style="margin-top: 10px"><span>{{articleInfo.create_time}}</span></el-row>
+                </el-col>
+
+                <el-col :span="3">
+                  <el-button type="success" size="medium" style="margin-top: 28px">{{buttonInfo}}已关注</el-button>
+                </el-col>
               </el-row>
-              <div class="text">
+
+              <div class="text" style="width: 705px">
+                <!--                图片-->
                 <div class="text-img">
                   <el-image
-                    style="width: 100px; height: 100px"
+                    style="width: 100%; height: 100%;"
                     src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-                    fit="contain"></el-image>
+                    fit="fill"></el-image>
                 </div>
+                <!--                标题-->
                 <div class="text-title">
                   <b>{{articleInfo.title}}</b>
                 </div>
-                <div class="text-inside">
+                <!--                主题内容-->
+                <div class="text-content">
                   <span>{{articleInfo.content}}</span>
                 </div>
               </div>
+
+
             </div>
           </el-col>
-          <el-col :span="6">
-            第二列sdsa
+          <el-col :span="5" style="height: 100%">
+            <div style="background-color: blueviolet" class="main-right">右侧</div>
           </el-col>
         </el-row>
-      </div>
-    </el-main>
-
+      </el-main>
+    </div>
   </el-container>
 </template>
 
@@ -85,7 +77,7 @@
         },
         authorInfo: {
           author_id: '',
-          name: ''
+          author_name: ''
         },
         is_attention: '',
         buttonInfo: ''
@@ -105,7 +97,7 @@
         axios.defaults.baseURL = '';
         const {data: res} = await axios.get(`http://localhost:9003/article/detail/${this.authorInfo.author_id}/${this.articleInfo.article_id}`);
         if (res.flag) {
-          this.authorInfo.name = res.data.author_name;
+          this.authorInfo.author_name = res.data.author_name;
           this.articleInfo.title = res.data.title;
           this.articleInfo.content = res.data.content;
           this.articleInfo.create_time = res.data.create_time
@@ -119,46 +111,51 @@
 </script>
 
 <style scoped lang="scss">
-  .home {
+  .main {
     background-color: #eeeeee;
     height: 100%;
   }
 
-  .main-div {
-    margin-left: 20%;
-    margin-right: 20%;
+  .main-box {
+    margin-left: 200px;
+    margin-right: 250px;
+    background-color: #eeeeee;
     height: 100%;
   }
 
-  .main {
+  .main-left {
+    text-align: center;
     height: 100%;
   }
 
-  .user-header {
-    float: left;
-  }
-
-  .header {
+  .main-center {
+    height: 100%;
     background-color: #ffffff;
+  }
+
+  .main-right {
     height: 100%;
   }
 
-  .text {
+  .center-header {
+    margin-top: 10px;
+    width: 66px;
+    height: 66px;
+  }
+
+  .text-img {
+    padding: 5px;
+    width: 60%;
+    margin: 0 auto
+  }
+
+  .text-title {
+    margin-top: 30px;
+    margin-left: 30px;
+  }
+
+  .text-content {
     margin-top: 20px;
-
-    .text-img {
-      margin-left: 45%;
-    }
-
-    .text-inside {
-      margin-top: 30px;
-      margin-left: 15%;
-    }
-
-    .text-title {
-      margin-top: 50px;
-      margin-left: 10%;
-      font-size: large;
-    }
+    margin-left: 30px;
   }
 </style>
---
+++ src/components/Gathering/Gathering.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "Gathering"
+    }
+</script>
+
+<style scoped>
+
+</style>
