009e448cb88ba7988ffc5b6944f52b460004a47b
sundy8336 <sundy8336@fxiaoke.com>
2020-03-13 01:53:49 +0800

    feat 评论页面

--- src/components/Main/Spit.vue
+++ src/components/Main/Spit.vue
@@ -1,87 +1,59 @@
 <template>
-  <div class="edit-box">
-    <div class="text_box" style="width: 100%;display: flex;justify-content: center;">
-      <vue-edit ref="editor" id="editor" v-model="initContent" :menus="menus"></vue-edit>
-    </div>
-    <div class="btn" style="margin-top: 30px;">
-      <el-button type="primary" @click="setContent">设置富文本内容</el-button>
-      <el-button type="primary" @click="getContent">获取富文本内容</el-button>
-      <el-button type="primary" @click="goNextPage">跳转页面</el-button>
-    </div>
-  </div>
+  <el-container>
+    <el-header>
+      <el-card class="edit-box" style="float: left;">
+        <textarea style="height: 100px; width: 100%;"></textarea>
+        <el-button style="width: 100%">发布</el-button>
+      </el-card>
+      <el-card style="margin-top: 60px; width: 200px; height: 200px;">
+        个人信息
+      </el-card>
+    </el-header>
+    <el-main style="margin-top: 200px; width: 640px; margin-left: 24.5%">
+      <el-row v-for="item in 40" :key="item">
+        <el-card>
+          一条吐槽
+          <el-button >展开回复</el-button>
+          <el-button >评论</el-button>
+          <el-row v-for="item in 10" :key="item">
+            <el-divider></el-divider>
+            <el-col>
+              我是评论
+            </el-col>
+          </el-row>
+        </el-card>
+
+      </el-row>
+
+
+
+    </el-main>
+  </el-container>
+
+
 </template>
 <script>
   //参考文档 https://www.npmjs.com/package/vue-wangeditor
   import vueEdit from 'vue-wangeditor'
+
   export default {
+    isShow:'show',
+    methods:{
+    },
     components: {
       vueEdit
     },
     data() {
-      return {
-        initContent: '',
-        menus: [
-          'source', // 源码模式
-          '|',
-          'bold', // 粗体
-          'underline', // 下划线
-          'italic', // 斜体
-          'strikethrough', // 中线
-          'eraser', // 清空格式
-          'forecolor', // 文字颜色
-          'bgcolor', // 背景颜色
-          '|',
-          'quote', // 引用
-          'fontfamily', // 字体
-          'fontsize', // 字号
-          'head', // 标题
-          'unorderlist', // 无序列表
-          'orderlist', // 有序列表
-          'alignleft', // 左对齐
-          'aligncenter', // 居中
-          'alignright', // 右对齐
-          '|',
-          'link', // 链接
-          'unlink', // 取消链接
-          'table', // 表格
-          'emotion', // 表情
-          '|',
-          'img', // 图片
-          'video', // 视频
-          'insertcode', // 插入代码
-          '|',
-          'undo', // 撤销
-          'redo', // 重做
-          'fullscreen' // 全屏
-        ]
-      }
+      return {}
     },
-    mounted() {
-      console.log(this.$refs.editor, '富文本实例')
-    },
-    methods: {
-      setContent() {
-        this.initContent = '设置好的内容';
-        this.$refs.editor.setHtml(this.initContent) //如设置：后台返回来的固定内容
-        console.log(this.initContent, '设置编辑器内容')
-      },
-      getContent() {
-        this.initContent = this.$refs.editor.getHtml(this.initContent);
-        console.log(this.initContent, '获取编辑器当前内容的html代码片段')
-      },
-      //vue 跳转
-      goNextPage() {
-        this.$router.push({
-          name: 'testEdit',
-        })
-      }
-    }
   }
-
 </script>
 
 <style scoped lang="scss">
-  .edit-box{
-    margin-top: 100px;
+  .edit-box {
+    margin-top: 60px;
+    margin-left: 25%;
+    height: 200px;
+    width: 600px;
   }
 </style>
