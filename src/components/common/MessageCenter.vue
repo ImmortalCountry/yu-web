6cb6195c7b0c17ccf12751da411fb595861fb2b8
sundy8336 <sundy8336@fxiaoke.com>
2020-05-05 17:50:48 +0800

    fix 显示

--- src/components/common/MessageCenter.vue
+++ src/components/common/MessageCenter.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    消息中心
+  <div class="home">
+    <el-card class="box-card">
+      <div v-for="item in messageList" :key="item.id" class="text item">
+        <el-divider></el-divider>
+
+        <span>{{$commonUtils.timeTrans(item.createTime)}}</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>{{item.senderName}}</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>{{item.action}}</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>给你：</span>
+        <el-divider direction="vertical"></el-divider>
+        <span>{{item.content}}</span>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -16,7 +30,7 @@
     },
     methods: {
       getMessage() {
-        this.$message.notify.getMessageList().then(res => {
+        this.$api.notify.getMessageList().then(res => {
           if (res.flag) {
             this.messageList = res.data;
           }
@@ -27,5 +41,16 @@
 </script>
 
 <style scoped lang="scss">
+  .text {
+    font-size: 14px;
+  }
 
+  .item {
+    padding: 18px 0;
+  }
+
+  .box-card {
+    width: 480px;
+    margin-left: 110px;
+  }
 </style>
